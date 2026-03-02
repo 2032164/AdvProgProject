85323dbfc33ab8c93ad57e7584271bd447f13ec0
greenwinner <122694816+greenwinner@users.noreply.github.com>
2026-03-01 17:28:11 -0800

    Added Some models and pushable objects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -73,4 +73,20 @@ public class FPSController : MonoBehaviour
         }
 
     }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * walkSpeed;
+    }
 }
