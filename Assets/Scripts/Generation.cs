13ea903c812a69e8ba416b5439e5a082d02d5323
Liam Napier <2032164@apps.nsd.org>
2026-03-02 14:21:27 -0800

    Added more rooms and started better generation

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class Generation : MonoBehaviour
 {
     // Start is called before the first frame update
-    //offset is 8.27f
-    public GameObject[] rooms;
+    //offset is 10?
+    public GameObject[] rooms;//1= start, 2= straight, 3= left turn, 4 = right turn
+    public int numRooms 10f;
+    private GameObject[][] map;
+    private int rotation;
+
+
+    //every left turn = +90 deg of rotation and z+
+    //every right turn = -90 deg of rotation and z-
+
+ 
     void Start()
     {
-        for(int i = 0; i < rooms.Length; i++)
+        Instantiate(rooms[1], new Vector3(0, 0, 0), Quaternion.identity);
+        for(int i = 1; i < numRooms; i++)
         {
-            Instantiate(rooms[i], new Vector3(8.27f * i, 0, 0), Quaternion.identity);
+            int rand = Random.range(1,rooms.length)
+            GameObject temp = rooms[rand]
+
+            //Instantiate(rooms[i], new Vector3(10f * i, 0, 0), Quaternion.identity);
         }
     }
 }
