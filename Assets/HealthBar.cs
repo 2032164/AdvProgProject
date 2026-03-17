e2e1bb4399d6d8ad7a7863fcceb7c2aea5d0708d
Liam Napier <2032164@apps.nsd.org>
2026-03-17 11:16:45 -0700

    Update HealthBar.cs

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public bool healing;
+    public float maxHealth;
+    private bool healAmount = .01f;
     // Start is called before the first frame update
     [SerializeField]
     private Image healthBarFill;
     void Start()
     {
+        //need to figure out how to make the fill amount scale to the right full health if that makes sense
         healthBarFill.fillAmount = .5F;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBarFill.fillAmount += .01F;
+        healthBarFill.fillAmount += healAmount;
     }
 }
