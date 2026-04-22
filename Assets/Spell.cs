4e83f4a4b50ab07092f8b2309952a04c307b155f
Liam Napier <2032164@apps.nsd.org>
2026-04-22 11:09:01 -0700

    Update Spell.cs

--- Assets/Spell.cs
+++ Assets/Spell.cs
@@ -8,13 +8,14 @@ public class Spell : MonoBehaviour
 {
     // Start is called before the first frame update
     public int rarity;//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
-    public Color[] spellColors = new Color[5];//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
+    public Color[] rarityColors = {Color.grey,Color.green,Color.blue,Color.purple,Color.orange};//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
     public int spellType;//each will correspond to a different spell effect and bottom gem color
-    void Start()
+    public Color[] typeColors = {Color.red,Color.brown, Color.aquamarine,Color.grey};//fire,earth,water,wind?
+    void Start(int rarity,int spellType)
     {
         //need to make it so that first time it gets cast it creates a random spell and then after that it just casts the spell
-        rarity = Random.Range(1, 6); // Assuming rarity values are 1-5
-        spellType = Random.Range(1, 6); // Assuming spellType values are 1-5
+        this.rarity = rarity;
+        this.spellType = spellType;
 
     }
 
@@ -22,5 +23,11 @@ public class Spell : MonoBehaviour
     void Update()
     {
         
+    }
+    public Color topGem(){
+        return rarityColors[rarity];
+    }
+    public Color bottomGem(){
+        return typeColors[spellType];
     }
 }
