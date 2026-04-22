c27542d6e3123ab06809a7a4174a917cb0e845e9
Liam Napier <2032164@apps.nsd.org>
2026-04-22 11:14:03 -0700

    Update Spell.cs

--- Assets/Spell.cs
+++ Assets/Spell.cs
@@ -7,27 +7,21 @@ using Color = UnityEngine.Color;
 public class Spell : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int rarity;//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
+    public Color rarity;//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
     public Color[] rarityColors = {Color.grey,Color.green,Color.blue,Color.purple,Color.orange};//each will correspond to a different spell type and top gem color [grey,green,blue,purple,orange]
-    public int spellType;//each will correspond to a different spell effect and bottom gem color
+    public Color spellType;//each will correspond to a different spell effect and bottom gem color
     public Color[] typeColors = {Color.red,Color.brown, Color.aquamarine,Color.grey};//fire,earth,water,wind?
     void Start(int rarity,int spellType)
     {
         //need to make it so that first time it gets cast it creates a random spell and then after that it just casts the spell
-        this.rarity = rarity;
-        this.spellType = spellType;
-
+        rarity = rarityColors[rarity];
+        spellType = typeColors[spellType];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //nothing
     }
-    public Color topGem(){
-        return rarityColors[rarity];
-    }
-    public Color bottomGem(){
-        return typeColors[spellType];
-    }
+
 }
