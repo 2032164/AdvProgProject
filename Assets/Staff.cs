f8ad3d4aa0f9be26a5c16b78e2eb07dcb83dbf26
Liam Napier <2032164@apps.nsd.org>
2026-04-22 11:12:57 -0700

    Updated Staff.cs
    Added topGem and bottomGem game objects to Staff class. Updated spell initialization and gem color assignment in Start and Update methods.

--- Assets/Staff.cs
+++ Assets/Staff.cs
@@ -8,10 +8,14 @@ public class Staff : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform spellGen;
+    public gameObject topGem;
+    public gameObject bottomGem;
     private Spell spell;
     void Start()
     {
-        
+        spell = new Spell(Random.range(0,5),Random.range(0,4));//need to change it so higher rarities are rarer
+        topGem.GetComponent<Renderer>().material.color = spell.topGem;
+        bottomGem.GetComponent<Renderer>().material.color = spell.bottomGem; 
     }
 
     // Update is called once per frame
@@ -20,7 +24,11 @@ public class Staff : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouse Clicked");
-            //spell.Cast(spellGen);
+            spell = new Spell(Random.range(0,5),Random.range(0,4));//need to change it so higher rarities are rarer
+            topGem.GetComponent<Renderer>().material.color = spell.topGem;
+            bottomGem.GetComponent<Renderer>().material.color = spell.bottomGem; 
+
         }
     }
+    
 }
